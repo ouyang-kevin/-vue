1238586e60b5e80372fefc0ca1ae8c7d36fc04b4
ouyang-kevin <1572620397@qq.com>
2021-09-01 15:31:10 +0800

    props属性(传数据)

---
+++ 3_src_props属性/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div> 
+    <h2>{{msg}}</h2>
+    <hr>
+    <Student name='张三' sex="男" :age="18"></Student>
+  </div>
+</template>
+
+<script>
+// 引入组件
+import Student from "./components/Student.vue";
+
+export default {
+  name: "App",
+  components: {
+    Student,
+  },
+  data() {
+    return {
+      msg: '这是展示的一些信息'
+    }
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ 3_src_props属性/components/Student.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h2>学生姓名:{{ name }}</h2>
+    <h2>学生年龄:{{ TheAge }}</h2>
+    <h2>学生性别:{{ sex }}</h2>
+    <button @click="addAge">点我年龄加一</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Student",
+  //  简单声明接收   props接收的数据级别高于本地data中定义的数据级别
+  // props: ["name", "age", 'sex'], 
+  // 接收的同时对数据进行类型限制
+  // props:{
+  //   name:String,
+  //   age: Number,
+  //   sex:String
+  // },
+  // 完整声明接收
+  props: {
+    name: {
+      type: String, //类型要求
+      required: true, //要求必传的数据
+    },
+    age: {
+      type: Number,
+      default: 17  //默认数据值; 与required两者只能出现一个
+    },
+    sex: {
+      type: String,
+      required: true,
+    }
+  },
+  data() {
+    return {
+      TheAge: this.age,
+    };
+  },
+  methods: {
+    addAge() {
+      this.TheAge++;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
