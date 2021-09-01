188d521cb909da001aba7c4943d4c120358a9694
ouyang-kevin <1572620397@qq.com>
2021-09-01 15:33:18 +0800

    动画和过渡效果

---
+++ 8_src_过渡与动画/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app">
+    <Test/>
+  </div>
+</template>
+
+<script>
+// 引入组件
+import Test from "./components/Test.vue";
+
+export default {
+  name: "App",
+  components: {
+    Test,
+  },
+};
+</script>
+
+<style scoped>
+.app {
+  background-color: #eee;
+}
+</style>
---
+++ 8_src_过渡与动画/components/Test.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="Test">
+    <button @click="isShow = !isShow">显示与隐藏</button>
+    <!-- 
+      vue中使用配置的动画
+    transition只能配置一组html标签页
+    transition-group能配置多组html标签页
+    appear是让动画自行进入
+     -->
+    <transition-group name="hello" appear>
+      <h1 v-show="isShow" key="1">{{ info }}</h1>
+      <h1 v-show="!isShow" key="2">{{ infos }}</h1>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data() {
+    return {
+      info: "你好啊!",
+      infos: "你好啊!世界",
+      isShow: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.Test {
+  margin: 30px 0;
+}
+h1 {
+  background-color: orange;
+}
+/* 进入的动画属性 */
+.hello-enter-active {
+  animation: atguigu 0.5s linear;
+}
+/* 离开的动画属性 */
+.hello-leave-active {
+  animation: atguigu 0.5s linear reverse;
+}
+/* 配置动画名为atguigu */
+@keyframes atguigu {
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0px);
+  }
+}
+</style>
