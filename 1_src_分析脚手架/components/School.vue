e603da664dc418c9eb9b65a003b178adceb9c887
ouyang-kevin <1572620397@qq.com>
2021-09-01 15:29:08 +0800

    分析vue-cli

---
+++ 1_src_分析脚手架/components/School.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <h2>{{ address }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'School',
+  data() {
+    return {
+      name: "尚硅谷",
+      address: "北京",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ 1_src_分析脚手架/components/Student.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>{{ name }}</h2>
+    <h2>{{ age }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Student',
+  data() {
+    return {
+      name: "李明",
+      age: 15
+    };
+  },
+};
+</script>
+
+<style>
+</style>
